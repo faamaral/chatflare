14427a86688ac3de3b068de28de2c83ac1561282
faamaral <fabianoamaral445@gmail.com>
2023-03-27 21:10:41 -0300

    created bubble messages

---
+++ lib/components/message_bubble.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+
+import 'package:chatflare/core/models/chat_message.dart';
+import 'package:chatflare/utils/constants/app_images.dart';
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final ChatMessage message;
+  final bool belongsToCurrentUser;
+  const MessageBubble(
+      {super.key, required this.message, required this.belongsToCurrentUser});
+
+  Widget _showUserImage(String imageUrl) {
+    ImageProvider? provider;
+    final uri = Uri.parse(imageUrl);
+    if (uri.path.contains(AppImages.avatar)) {
+      provider = const AssetImage(AppImages.avatar);
+    } else if (uri.scheme.contains('http')) {
+      provider = NetworkImage(uri.toString());
+    } else {
+      provider = FileImage(File(uri.toString()));
+    }
+    return CircleAvatar(
+      backgroundImage: provider,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          mainAxisAlignment: belongsToCurrentUser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: belongsToCurrentUser
+                    ? Colors.grey.shade300
+                    : Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(12),
+                  topRight: const Radius.circular(12),
+                  bottomLeft: belongsToCurrentUser
+                      ? const Radius.circular(12)
+                      : const Radius.circular(0),
+                  bottomRight: belongsToCurrentUser
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
+                ),
+              ),
+              width: 180,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: belongsToCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                    ),
+                  ),
+                  Text(
+                    message.text,
+                    textAlign: belongsToCurrentUser ? TextAlign.right : TextAlign.left,
+                    style: TextStyle(
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: belongsToCurrentUser ? null : 165,
+          right: belongsToCurrentUser ? 165 : null,
+          child: _showUserImage(message.userImageUrl),
+        ),
+      ],
+    );
+  }
+}
--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,7 @@
+import 'package:chatflare/components/message_bubble.dart';
+import 'package:chatflare/core/models/chat_message.dart';
+import 'package:chatflare/core/services/auth/auth_service.dart';
+import 'package:chatflare/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,6 +9,31 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('New Mensage'),);
+    final currentUser = AuthService().currentUser;
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(
+            child: Text('Sem dados'),
+          );
+        } else {
+          final msgs = snapshot.data!;
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs.length,
+            itemBuilder: (context, index) => MessageBubble(
+              key: ValueKey(msgs[index].id),
+              message: msgs[index],
+              belongsToCurrentUser: currentUser?.id == msgs[index].userId,
+            ),
+          );
+        }
+      },
+    );
   }
 }
