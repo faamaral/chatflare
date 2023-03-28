340b2b9754cde83d4aad5da8b2d66dae44b21185
faamaral <fabianoamaral445@gmail.com>
2023-03-28 17:29:34 -0300

    setting background and app finished push notifications

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -24,6 +24,8 @@ class ChatNoticationService with ChangeNotifier {
   // Push notification
   Future<void> init() async {
     await _configureForeground();
+    await _configureBackground();
+    await _configureTerminated();
   }
 
   Future<bool> get _isAuthorized async {
@@ -35,12 +37,28 @@ class ChatNoticationService with ChangeNotifier {
 
   Future<void> _configureForeground() async {
     if (await _isAuthorized) {
-      FirebaseMessaging.onMessage.listen((msg) {
-        if (msg == null) return;
-        add(ChatNotification(
-            title: msg.notification!.title ?? 'Não informado',
-            body: msg.notification!.title ?? 'Não informado'));
-      });
+      FirebaseMessaging.onMessage.listen(_messageHandler);
     }
+  }
+
+  Future<void> _configureBackground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureTerminated() async {
+    if (await _isAuthorized) {
+      RemoteMessage? initialMsg =
+          await FirebaseMessaging.instance.getInitialMessage();
+      _messageHandler(initialMsg);
+    }
+  }
+
+  void _messageHandler(RemoteMessage? msg) {
+    if (msg == null || msg.notification == null) return;
+    add(ChatNotification(
+        title: msg.notification!.title ?? 'Não informado',
+        body: msg.notification!.body ?? 'Não informado'));
   }
 }
